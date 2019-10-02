03e44c2d10201319e68fb270ff4d19a3d41b5073
100111079 <kylervillarreal@icloud.com>
2019-10-02 09:02:04 -0500

    Octothorp comment

---
+++ learning.py
@@ -0,0 +1,25 @@
+print("hello world")
+print("howdy, y'alL")
+print("I like typing this")
+
+# math skills demo
+
+print(" i will not count my chickens", 2)
+print("Hens: ", 25+30/6)
+print("Roosters: ", 100 - 25 * 3 % 4)
+print("Now I will count the eggs, 7")
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 +6)
+
+# more math problems
+
+print(5.0 + 4.0 + 1.0 + 7.0 + 9.0)
+print(99.0 - 18.0 - 60.0 + 50.0 + 33.0)
+print(100.0 + 500.0 - 400.0 - 150.0 + 200.0 - 10.0 + 70.0)
+
+
+
+
+
+
+
+
