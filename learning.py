759ca368fdd6619fd4f6197ea122441c1232b6a2
100111079 <kylervillarreal@icloud.com>
2019-10-04 09:02:28 -0500

    Variables and

--- learning.py
+++ learning.py
@@ -16,6 +16,47 @@ print(5.0 + 4.0 + 1.0 + 7.0 + 9.0)
 print(99.0 - 18.0 - 60.0 + 50.0 + 33.0)
 print(100.0 + 500.0 - 400.0 - 150.0 + 200.0 - 10.0 + 70.0)
 
+# Variables and some of their powers
+cars = 80
+SpaceInACar = 4.0
+drivers = 45
+passengers = 115
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = SpaceInACar * cars_driven
+average_passenegers_per_car = passengers / cars_driven
+
+print("There are", cars, "cars avaliable")
+print("There are only", drivers, "drivers avaliable today")
+print("There will be", cars_not_driven, "empty cars today")
+print("We can transport", carpool_capacity, "people today")
+print("We have" , passengers, "to carpool today")
+
+
+# More variables and playing with output
+myName = "Kyler Villarreal"
+myAge = 100
+myHeight = 72 # inches
+myEyes = "Brown"
+myHair = "yes, some"
+
+print("Let's talk about %s." % myName)
+print("He's %d inches tall." % myHeight)
+print("He's got %d eyes and %s hair." % myEyes, myHair)
+
+myWeight = 140 # pounds
+myLegs = "Long"
+myHands = 5 # inches
+myHead = "Medium"
+myChest = 11 # inches
+
+print("He weighs %d pounds." % myWeight)
+print("His legs are %s." % myLegs)
+print("His hands are %s inches long." % myHands)
+print("His head is %s inches long." % myHead)
+print("His chest is %f inches wide." % myChest)
+
+
 
 
 
